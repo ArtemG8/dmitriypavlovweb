aa33c031cb470c65591f93b4e8c8dd3f47e92aeb
ArtemG8 <120787936+ArtemG8@users.noreply.github.com>
2024-12-02 21:52:09 +0300

    feat: prepare for deploy

--- pavlov_web/pavlov_web/settings.py
+++ pavlov_web/pavlov_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2j!i#=7(^0tk0_y2ws%e#1y=7@9&@kt56sj*@dwka0p_f1u5%#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
